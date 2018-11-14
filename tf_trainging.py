a1da0e830a6f2740abe3e80ec5ceeaa9caea2c59
Alfonso <ajzuniga1@miuandes.cl>
2018-11-13 21:04:01 -0300

    Working

--- tf_trainging.py
+++ tf_trainging.py
@@ -5,8 +5,5 @@ import time
 training_csv = 'training_set.csv'
 training_dataset = tf.data.experimental.make_csv_dataset(training_csv, batch_size=32)
 iter = training_dataset.make_one_shot_iterator()
-next = iter.get_next()
-print(next)  # next is a dict with key=columns names and value=column data
-'''inputs, labels = next['text'], next['sentiment']
-with tf.Session() as sess:
-    sess.run([inputs, labels])'''
+print(iter.get_next())
+print(iter.get_next())
